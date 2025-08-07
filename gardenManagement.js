995c976d9dd0c43a344292ece745bc83b103aeb4
bymontana <montanahayes@mail.com>
2025-08-07 19:36:41 -0400

    Inital code for statement lab

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,21 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+if (temperature > 80) {
+    console.log("Watering on");
+}
+else if {
+console.log("Watering off");
+}
+
+if (timeOfDay === "evening" || timeOfDay === "night") {
+    console.log("Lights on);
+} else {
+    console.log("Lights off");         
+}
+
+while (soilMoisture < 40) {
+    console.log(soilMositure);
+    soilMoisture ++ 5;
+}
